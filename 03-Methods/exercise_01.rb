e92a47d6448d9900c5760f27a76d9ceedf0385f1
Lori Sanders <lori.sanders@me.com>
2019-03-27 16:34:51 +0100

    Exercises completed 27 March 2019.

---
+++ 03-Methods/exercise_01.rb
@@ -0,0 +1,9 @@
+=begin
+Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string.
+=end
+
+def greeting(name)
+  hello = "Hello, #{name}!"
+end
+
+puts greeting("Lori")
